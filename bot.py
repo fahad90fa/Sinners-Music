f99413bcbfd3b8e4dab9b7b533955604831ffbf9
BlackHat <fahad1562009@gmail.com>
2026-04-07 23:53:06 +0500

    add osint

--- bot.py
+++ bot.py
@@ -75,7 +75,7 @@ async def on_ready():
 
 
 async def load_cogs():
-    cogs = ['cogs.setup', 'cogs.products', 'cogs.tickets', 'cogs.admin', 'cogs.rules', 'cogs.welcome', 'cogs.data']
+    cogs = ['cogs.setup', 'cogs.products', 'cogs.tickets', 'cogs.admin', 'cogs.rules', 'cogs.welcome']
     for cog in cogs:
         try:
             await bot.load_extension(cog)
